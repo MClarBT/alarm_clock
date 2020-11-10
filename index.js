e15474650c8f06a9a0b04fdf14294963bf5aee7a
MClarBT <buchm751@gmail.com>
2020-11-09 19:53:44 -0500

    Finding different way to have the user input the time for alarm

--- index.js
+++ index.js
@@ -22,6 +22,34 @@ window.addEventListener('load', function() {
 
   //Next TODO: I need to make a method that catches the time the user wants an alarm for and then
   //actually acts as an alarm
+  //I'll need the method to make sure the minutes are anywhere from 0-60 inclusive.
+  //I confess I'm having a hard time doing this, but maybe if I did something
+  //like this:
+
+  /*
+  function setAlarm() {
+    var ms = document.getElementById('alarmTime').valueAsNumber;
+    if(isNaN(ms)) {
+      alert('Invalid Date');
+      return;
+  }
+
+  var alarm = new Date(ms);
+
+  var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(),....)
+
+  (refer to Alarm Clock in JS | JavaScript Tutorials | Web Development Tutorials video
+  by VerkkoNet on YouTube)
+}
+  */
+
+
+  /*function catchTimeForAlarm() {
+
+
+
+  }*/
+
 
   event.preventDefault();
   getTime();
