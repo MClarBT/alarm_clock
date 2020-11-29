1d8af39e201a869447718863fd8e3887a6a3ff38
MClarBT <buchm751@gmail.com>
2020-11-29 13:17:01 -0500

    Added a few functions, popup saying Invalid Date

--- index.js
+++ index.js
@@ -22,12 +22,14 @@ window.addEventListener('load', function() {
 
   //Next TODO: I need to make a method that catches the time the user wants an alarm for and then
   //actually acts as an alarm
-  //I'll need the method to make sure the minutes are anywhere from 0-60 inclusive.
+  //I'll need the method to make sure the minutes are anywhere from 0-60 inclusive (exclusive?)
   //I confess I'm having a hard time doing this, but maybe if I did something
   //like this:
 
-  /*
-  function setAlarm() {
+  var alarmSound = new Audio();
+  alarmSound.src = 'alarm.mp3;'
+
+  function setAlarm(button) {
     var ms = document.getElementById('alarmTime').valueAsNumber;
     if(isNaN(ms)) {
       alert('Invalid Date');
@@ -36,22 +38,56 @@ window.addEventListener('load', function() {
 
   var alarm = new Date(ms);
 
-  var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(),....)
+  var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
 
-  (refer to Alarm Clock in JS | JavaScript Tutorials | Web Development Tutorials video
-  by VerkkoNet on YouTube)
+  var differenceInMs = alarm.getTime() - (new Date()).getTime();
+
+  if(differenceInMs < 0)
+  {
+    alert('Specified time is already passed!');
+    return;
+  }
+
+  setTimeout(initAlarm, differenceInMs);
+
+  button.innerText = 'Cancel Alarm';
+  button.setAttribute('onclick', 'cancelAlarm(this);');
+
+
+
+};
+
+
+function cancelAlarm(button) {
+  button.innerText = 'Set Alarm';
+  button.setAttribute('onclick', 'setAlarm(this);');
+};
+
+
+function initAlarm() {
+    alarmSound.play();
+    document.getElementById('alarmOptions').style.display = '';
+};
+
+
+function stopAlarm() {
+  alarmSound.pause();
+  alarmSound.currentTime = 0;
+  document.getElementById('alarmOptions').style.display = 'none';
+};
+
+
+function snooze() {
+  stopAlarm();
+  setTimeout(initAlarm, 36000);
 }
-  */
 
-
-  /*function catchTimeForAlarm() {
-
-
-
-  }*/
-
+//(refer to Alarm Clock in JS | JavaScript Tutorials | Web Development Tutorials video by VerkkoNet on YouTube)
+//I'm following that video tutorial...not sure if I can commit anything to GitHub since my code isn't doing much as of now...
+//but maybe it would look better if I did...
 
   event.preventDefault();
   getTime();
+  setAlarm();
 
 });
