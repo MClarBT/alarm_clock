f17ed249ba232adfee91d3e15e4d1d76bd5854e3
MClarBT <buchm751@gmail.com>
2020-12-14 16:54:39 -0500

    Finally fixed that uncaught reference error about the setAlarm function

--- index.js
+++ index.js
@@ -1,59 +1,33 @@
 //Okay, so this will be the JavaScript file for the alarm clock...
 
-window.addEventListener('load', function() {
+var alarmSound = new Audio();
+alarmSound.src = 'alarm.mp3;'
+var alarmTimer;
 
-  function getTime() {
-    //okay, so the JavaScript way to get the time...
-
-    var today = new Date();
-
-    if(today.getHours() > 12)
-    {
-      var time = (today.getHours() - 12) + ":" + today.getMinutes();
-    }
-
-    else {
-      var time = today.getHours() + ":" + today.getMinutes();
-    }
-
-    document.getElementById("currentTime").innerHTML = time; //time.toLocaleTimeString();
-
-  }
-
-  //Next TODO: I need to make a method that catches the time the user wants an alarm for and then
-  //actually acts as an alarm
-  //I'll need the method to make sure the minutes are anywhere from 0-60 inclusive (exclusive?)
-  //I confess I'm having a hard time doing this, but maybe if I did something
-  //like this:
-
-  var alarmSound = new Audio();
-  alarmSound.src = 'alarm.mp3;'
-
-  function setAlarm(button) {
-    var ms = document.getElementById('alarmTime').valueAsNumber;
-    if(isNaN(ms)) {
-      alert('Invalid Date');
-      return;
-  }
-
-  var alarm = new Date(ms);
-
-  var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
-
-  var differenceInMs = alarm.getTime() - (new Date()).getTime();
-
-  if(differenceInMs < 0)
-  {
-    alert('Specified time is already passed!');
+//putting this setAlarm function here to fix the uncaught reference error my code is complaining about
+function setAlarm(button) {
+  var ms = document.getElementById('alarmTime').valueAsNumber;
+  if(isNaN(ms)) {
+    alert('Invalid Date');
     return;
-  }
+}
 
-  setTimeout(initAlarm, differenceInMs);
+var alarm = new Date(ms);
 
-  button.innerText = 'Cancel Alarm';
-  button.setAttribute('onclick', 'cancelAlarm(this);');
+var alarmTime = new Date(alarm.getUTCFullYear(), alarm.getUTCMonth(), alarm.getUTCDate(), alarm.getUTCHours(), alarm.getUTCMinutes(), alarm.getUTCSeconds());
 
+var differenceInMs = alarm.getTime() - (new Date()).getTime();
 
+if(differenceInMs < 0)
+{
+  alert('Specified time is already passed!');
+  return;
+}
+
+alarmTimer = setTimeout(initAlarm, differenceInMs);
+
+button.innerText = 'Cancel Alarm';
+button.setAttribute('onclick', 'cancelAlarm(this);');
 
 };
 
@@ -61,6 +35,7 @@ window.addEventListener('load', function() {
 function cancelAlarm(button) {
   button.innerText = 'Set Alarm';
   button.setAttribute('onclick', 'setAlarm(this);');
+  clearTimeout(alarmTimer);
 };
 
 
@@ -79,15 +54,51 @@ function stopAlarm() {
 
 function snooze() {
   stopAlarm();
-  setTimeout(initAlarm, 36000);
+  setTimeout(initAlarm, 300000); // 5 * 60 * 1000 = 5 Minutes
 }
 
 //(refer to Alarm Clock in JS | JavaScript Tutorials | Web Development Tutorials video by VerkkoNet on YouTube)
-//I'm following that video tutorial...not sure if I can commit anything to GitHub since my code isn't doing much as of now...
-//but maybe it would look better if I did...
+//I'm following that video tutorial
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+window.addEventListener('load', function() {
+
+  function getTime() {
+    //okay, so the JavaScript way to get the time...
+
+    var today = new Date();
+
+    //I notice that when it's a time like 4:00 or 4:01, my webpage shows only 0 or 1 for the minutes...
+
+    //hours
+    if(today.getHours() > 12)
+    {
+      var time = (today.getHours() - 12) + ":" + today.getMinutes();
+    }
+
+    else {
+      var time = today.getHours() + ":" + today.getMinutes();
+    }
+
+    //Minutes
+    if(today.getMinutes() < 10)
+    {
+      //not sure if this will work...
+      str(today.getMinutes()) == "0" + str(today.getMinutes());
+    }
+
+    document.getElementById("currentTime").innerHTML = time; //time.toLocaleTimeString();
+
+  }
 
   event.preventDefault();
   getTime();
-  setAlarm();
+  //setAlarm();
 
 });
+
+
+//-------------------------------------------------------------------------------------------------------
